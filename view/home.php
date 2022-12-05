4c75c356eecafa7381d1b6571c0f5dae5853740a
Yacine667 <sid.yacinee@gmail.com>
2022-12-05 15:48:33 +0100

    rectif message d'erreur

--- view/home.php
+++ view/home.php
@@ -2,8 +2,20 @@
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
-<p>
-    <a href="index.php?ctrl=security&action=login">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="index.php?ctrl=security&action=addUser">S'inscrire</a>
-</p>
+<?php
+                        
+                        if(App\Session::getUser()){
+                            ?>
+                            <a href="/security/viewProfile.html"> <span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
+                            <a href="index.php?ctrl=security&action=logout">Déconnexion </a>
+                            <?php
+                        }
+                        else{
+                            ?>
+                            <a href="index.php?ctrl=security&action=login">Connexion </a>
+                            <a href="index.php?ctrl=security&action=addUser"> Inscription </a>
+                            
+                        <?php
+                        }                
+                        
+                    ?>
