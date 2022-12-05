1ed30bf3fa78055e90f7414a44900199724b0ee4
Yacine667 <sid.yacinee@gmail.com>
2022-12-05 08:57:56 +0100

    finalistion fonction addUser ( fonctionne )

--- forum1.sql
+++ forum1.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Hôte:                         127.0.0.1
--- Version du serveur:           5.7.33 - MySQL Community Server (GPL)
+-- Hôte :                        127.0.0.1
+-- Version du serveur:           5.7.24 - MySQL Community Server (GPL)
 -- SE du serveur:                Win64
--- HeidiSQL Version:             11.2.0.6213
+-- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,7 +10,6 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Listage de la structure de la base pour forum1
@@ -22,9 +21,15 @@ CREATE TABLE IF NOT EXISTS `categorie` (
   `id_categorie` int(11) NOT NULL AUTO_INCREMENT,
   `nomCategorie` varchar(50) NOT NULL,
   PRIMARY KEY (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table forum1.categorie : ~3 rows (environ)
+/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
+INSERT INTO `categorie` (`id_categorie`, `nomCategorie`) VALUES
+	(4, 'cat 1'),
+	(5, 'cat 2'),
+	(6, 'cat 3');
+/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
 
 -- Listage de la structure de la table forum1. post
 CREATE TABLE IF NOT EXISTS `post` (
@@ -38,9 +43,15 @@ CREATE TABLE IF NOT EXISTS `post` (
   KEY `topic_id` (`topic_id`),
   CONSTRAINT `FK_post_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`),
   CONSTRAINT `FK_post_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table forum1.post : ~0 rows (environ)
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+INSERT INTO `post` (`id_post`, `message`, `datePost`, `user_id`, `topic_id`) VALUES
+	(12, 'post 1', '2022-12-05 08:43:48', 4, 9),
+	(13, 'post 2', '2022-12-05 08:44:00', 5, 10),
+	(14, 'post 3', '2022-12-05 08:44:12', 6, 11);
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
 
 -- Listage de la structure de la table forum1. topic
 CREATE TABLE IF NOT EXISTS `topic` (
@@ -55,9 +66,15 @@ CREATE TABLE IF NOT EXISTS `topic` (
   KEY `categorie_id` (`categorie_id`),
   CONSTRAINT `topic_categorie` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id_categorie`),
   CONSTRAINT `topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table forum1.topic : ~0 rows (environ)
+/*!40000 ALTER TABLE `topic` DISABLE KEYS */;
+INSERT INTO `topic` (`id_topic`, `title`, `creationDate`, `closed`, `user_id`, `categorie_id`) VALUES
+	(9, 'topic 1', '2022-12-05 08:43:07', 0, 4, 4),
+	(10, 'topic 2', '2022-12-05 08:43:23', 0, 5, 5),
+	(11, 'topic 3', '2022-12-05 08:43:34', 0, 6, 6);
+/*!40000 ALTER TABLE `topic` ENABLE KEYS */;
 
 -- Listage de la structure de la table forum1. user
 CREATE TABLE IF NOT EXISTS `user` (
@@ -65,14 +82,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   `pseudo` varchar(255) NOT NULL,
   `mail` varchar(255) NOT NULL,
   `mdp` varchar(255) NOT NULL,
-  `dateInscription` datetime NOT NULL,
-  `role` varchar(50) NOT NULL,
+  `dateInscription` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Listage des données de la table forum1.user : ~0 rows (environ)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id_user`, `pseudo`, `mail`, `mdp`, `dateInscription`, `role`) VALUES
+	(4, 'user 1', 'user1@user.com', 'user1', '2022-12-05 08:41:55', 0),
+	(5, 'user 2', 'user2@user.com', 'user2', '2022-12-05 08:42:26', 0),
+	(6, 'user 3', 'user3@user.com', 'user3', '2022-12-05 08:42:47', 0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
